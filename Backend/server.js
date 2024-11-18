24de3031d4c0854ebc31a6210346580fa9a325af
Danijel Osovnikar <danijel.osovnikar@gmail.com>
2024-11-18 09:13:11 +0100

    changes in config

--- Backend/server.js
+++ Backend/server.js
@@ -8,9 +8,11 @@ import usersRoutes from "./routes/users.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
+import path from "path";
 
-// const app = express();
 const PORT = process.env.PORT || 3000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -20,6 +22,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", usersRoutes);
+
+app.use(express.static(path.join(__dirname, "/Frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
